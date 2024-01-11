8d688cae449bdc846f8930b0e3163535d27ecd45
Nahtaniel Iskandar <nathanieliskandarpro@gmail.com>
2024-01-11 23:24:13 +0700

    update ScrollReveal Effect

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,36 @@ import { Review } from "./components/ts/Review";
 import Contact from "./components/ts/Contact";
 import "./App.css";
 
-// Scroll Reveal Effect
-function scroll(direct: string, reset: boolean, tag: string, delay: number) {
-  const scrollEffect = ScrollReveal({
-    origin: direct,
-    distance: "50px",
-    duration: 2400,
-    reset: reset,
-  });
-
-  return scrollEffect.reveal(tag, { delay: delay });
-}
-
-scroll("top", false, "header", 100);
-scroll("left", false, ".main-text", 300);
-scroll("right", false, ".main-img", 300);
-scroll("bottom", false, ".shop-info", 300);
-scroll("top", false, ".about", 200);
-scroll("right", false, ".about-container > figure", 400);
-scroll("left", false, ".about-container > article", 400);
-scroll("bottom", false, ".shop", 300);
-scroll("top", false, ".review", 300);
-scroll("bottom", false, ".review-container", 400);
-scroll("top", false, "footer", 300);
+// Import useEffect for re-rendering ScrollReveal Effect
+import { useEffect } from "react";
 
 function App() {
+  // Scroll Reveal Effect
+  useEffect(() => {
+    function scroll(direct: string, reset: boolean, tag: string, delay: number) {
+      const scrollEffect = ScrollReveal({
+        origin: direct,
+        distance: "50px",
+        duration: 2400,
+        reset: reset,
+      });
+
+      return scrollEffect.reveal(tag, { delay: delay });
+    }
+
+    scroll("top", false, "header", 100);
+    scroll("left", false, ".main-text", 300);
+    scroll("right", false, ".main-img", 300);
+    scroll("bottom", false, ".shop-info", 300);
+    scroll("top", false, ".about", 200);
+    scroll("right", false, ".about-container > figure", 400);
+    scroll("left", false, ".about-container > article", 400);
+    scroll("bottom", false, ".shop", 300);
+    scroll("top", false, ".review", 300);
+    scroll("bottom", false, ".review-container", 400);
+    scroll("top", false, "footer", 300);
+  }, []);
+
   return (
     <>
       <header>
